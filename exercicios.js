c01fa692a3f02ad582dd58fbbe67719c390dd400
Aluno SENAI <Aluno@SN-351117.edu.fiesc.com.br>
2026-08-13 20:45:13 -0300

    maria

--- exercicios.js
+++ exercicios.js
@@ -19,10 +19,12 @@
 // Crie 2 variáveis com números e mostre no console
 // o resultado da soma, subtração, multiplicação e divisão.
 // ------------------------------------------------------------
-
-
-
-
+let numero1
+let numero2
+console.log(numero1 - numero2)
+console.log(numero1 + numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
 // EXERCÍCIO 2 - Conversor de temperatura
 // Crie uma variável com um valor em Celsius.
 // Converta para Fahrenheit e mostre no console.
